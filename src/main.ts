e60e3301060fe39dffea083fa51ec400a45f7181
zorbyte <zorbytee@gmail.com>
2020-05-26 15:59:03 +1000

    ci + docs: Add version, CI and better dev setup.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createLogger } from "./util/logger.ts";
+import { VERSION } from "../etc/version.ts";
 
 const log = createLogger();
 
-log.info("Hello template program!");
+log.info(`Hello template program! Current version: ${VERSION}.`);
